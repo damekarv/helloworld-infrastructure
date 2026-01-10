b9c7bb1f9907fd239d009d6f554f04f2df47db99
damekarv <damekarviney@gmail.com>
2026-01-10 18:16:29 +0100

    feat(vpc): Setup public route tables

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -72,3 +72,19 @@ resource "aws_nat_gateway" "this" {
   })
   depends_on = [aws_internet_gateway.this]
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+  tags = merge(local.tags, {
+    Name = "${local.name}-public"
+  })
+}
+resource "aws_route_table_association" "public" {
+  count = length(var.vpc_public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
