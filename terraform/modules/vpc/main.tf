eacaf44f293918ff76146268a8943564399f4a74
damekarv <damekarviney@gmail.com>
2026-01-10 18:10:32 +0100

    feat(vpc): Implement public subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -5,8 +5,12 @@ data "aws_availability_zones" "available" {
 locals {
   name = "${var.environment}-helloworld"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  
+  tags = merge({
+    created-by     = "DevOps Team"
+    Application    = "helloworld"
+  }, var.extra_tags)
 }
-
 
 resource "aws_vpc" "infra-vpc" {
   cidr_block           = var.vpc_cidr
@@ -16,4 +20,16 @@ resource "aws_vpc" "infra-vpc" {
   tags = {
     Name = "${local.name}-vpc"
   }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.vpc_public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.vpc_public_subnets[count.index]
+  availability_zone       = local.azs[count.index]
+  map_public_ip_on_launch = true
+  tags = merge(local.tags, {
+    Name                     = "${local.name}-public-${local.azs[count.index]}"
+    "kubernetes.io/role/elb" = 1
+  })
 }
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_public_subnets" {
+  description = "Public Subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+variable "extra_tags" {
+  description = "Extra tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
